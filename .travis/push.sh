9a85d6f21e1ede8793c94ea9148c513436e4a620
Matthias Klan <matthias.klan@gmail.com>
2018-05-29 11:20:02 +0200

    Create push.sh

---
+++ .travis/push.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#adapted from https://gist.github.com/willprice/e07efd73fb7f13f917ea thank you @willprice
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_pdf() {
+  git checkout -b master
+  git add . *.pdf
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/mklan/cv.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+setup_git
+commit_pdf
+upload_files
