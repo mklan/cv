dd2abd3efca0a0ded8b51fac6303063e7c744be8
Matthias Klan <matthias.klan@gmail.com>
2024-05-14 01:32:31 +0200

    initial

---
+++ .travis/push.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#adapted from https://gist.github.com/willprice/e07efd73fb7f13f917ea thank you @willprice
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit() {
+  git fetch
+  git checkout master
+  git add *.pdf *.png
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
+}
+
+upload_files() {
+  git remote add token_origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+  git push --quiet --set-upstream token_origin master
+}
+
+setup_git
+commit
+upload_files
---
+++ README.md
@@ -0,0 +1,15 @@
+![Curriculum Vitae of Matthias Klan](https://github.com/mklan/cv/blob/master/cv.png?raw=true)
+---
+
+[![CI](https://github.com/mklan/cv/actions/workflows/blank.yml/badge.svg)](https://github.com/mklan/cv/actions/workflows/blank.yml)
+
+[Download PDF](https://github.com/mklan/cv/raw/master/cv.pdf)
+
+You can fork this repo and use it as a framework for your personal CV.
+
+## Usage
+
+1. Fork
+2. Edit `cv.tex`
+3. Commit changes
+4. Wait for the pipeline to commit the rendered document
